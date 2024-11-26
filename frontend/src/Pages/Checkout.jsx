338f6939a62d2a065b0dea4ad5e7979550be7ac9
Deepak Jadon <167414676+deepakjadon1902@users.noreply.github.com>
2024-11-26 23:13:42 +0530

    update some files

--- frontend/src/Pages/Checkout.jsx
+++ frontend/src/Pages/Checkout.jsx
@@ -1,1 +1,13 @@
 
+
+import CheckoutPage from '../Components/CheckoutPage/CheckoutPage';
+
+const Checkout = () => {
+  return (
+    <div>
+      <CheckoutPage />
+    </div>
+  );
+};
+
+export default Checkout;
